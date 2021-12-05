58ae999c4001816a1983e5fdd373183d4bb575bd
kakamorsch <kakamorsch@gmail.com>
2021-12-05 07:52:00 -0300

    fixing the token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,12 +3,15 @@ const config = require("../config/config")
 
 
 const auth = (req, res, next) => {
-    const token_header = req.headers.auth;
+    const token_header = req.headers.authorization;
     if(!token_header) return res.status(401).send({error: "Autenticacao recusada!"})
 
     jwt.verify(token_header, config.jwtToken, (err, decoded) => {
-        if(err) res.status(401).send({error: "o token enviado nao eh valido"})
-        res.locals.authData = decoded
+        if(err) {
+            res.status(401).send({error: "o token enviado nao eh valido"})
+            return next();
+        }
+        res.locals.authData = decoded.id
         return next();
     })
 
